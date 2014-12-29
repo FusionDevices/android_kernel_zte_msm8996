c64e7c703ea21b23a0122faa58da9a9f02ae1852
Yao Dongdong <yaodongdong@huawei.com>
2014-12-29 14:41:43 +0800

    UPSTREAM: sched/core: Remove check of p->sched_class
    Search all usage of p->sched_class in sched/core.c, no one check it
    before use, so it seems that every task must belong to one sched_class.
    
    Signed-off-by: Yao Dongdong <yaodongdong@huawei.com>
    [ Moved the early class assignment to make it boot. ]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1419835303-28958-1-git-send-email-yaodongdong@huawei.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit 1b537c7d1e58c761212a193085f9049b58f672e6)
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    TEST=smaug-release and strago-release trybots.
    
    Change-Id: I5fa325b8657b7c1f976224ca3a2a174d3fbce660
    Reviewed-on: https://chromium-review.googlesource.com/321503
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4834,7 +4834,7 @@ void init_idle(struct task_struct *idle, int cpu)
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
-	if (p->sched_class && p->sched_class->set_cpus_allowed)
+	if (p->sched_class->set_cpus_allowed)
 		p->sched_class->set_cpus_allowed(p, new_mask);
 
 	cpumask_copy(&p->cpus_allowed, new_mask);

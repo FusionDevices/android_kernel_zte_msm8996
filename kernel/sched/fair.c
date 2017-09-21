5433c20860a8e55ef612d26758f2d057176534ff
Paul Ravenstone <sc4ryb3ar@gmail.com>
2017-09-21 14:28:57 -0400

    UPSTREAM: sched/fair: Fix new task's load avg removed from source CPU in wake_up_new_task()
    If a newly created task is selected to go to a different CPU in fork
    balance when it wakes up the first time, its load averages should
    not be removed from the source CPU since they are never added to
    it before. The same is also applicable to a never used group entity.
    
    Fix it in remove_entity_load_avg(): when entity's last_update_time
    is 0, simply return. This should precisely identify the case in
    question, because in other migrations, the last_update_time is set
    to 0 after remove_entity_load_avg().
    
    Reported-by: Steve Muckle <steve.muckle@linaro.org>
    Signed-off-by: Yuyang Du <yuyang.du@intel.com>
    [peterz: cfs_rq_last_update_time]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Juri Lelli <Juri.Lelli@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/20151216233427.GJ28098@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 0905f04eb21fc1c2e690bed5d0418a061d56c225)
    Signed-off-by: Javi Merino <javi.merino@arm.com>
    
    BUG=chrome-os-partner:47839
    TEST=Boot kernel on Oak.
    TEST=smaug-release and strago-release trybots.
    
    Change-Id: Ie21205c351b32cc8c0d5bac195085b1fa9e872fb
    Reviewed-on: https://chromium-review.googlesource.com/327028
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2890,25 +2890,48 @@ static inline void enqueue_entity_load_avg(struct cfs_rq *cfs_rq,
 		max_t(s64,  cfs_rq->runnable_load_sum - se->avg.load_sum, 0);
 }
 
-/*
- * Remove se's load from this cfs_rq child load-average, if the entity is
- * transitioning to a blocked state we track its projected decay using
- * blocked_load_avg.
- */
-static inline void dequeue_entity_load_avg(struct cfs_rq *cfs_rq,
-						  struct sched_entity *se,
-						  int sleep)
+#ifndef CONFIG_64BIT
+static inline u64 cfs_rq_last_update_time(struct cfs_rq *cfs_rq)
 {
-	update_entity_load_avg(se, 1);
-	/* we force update consideration on load-balancer moves */
-	update_cfs_rq_blocked_load(cfs_rq, !sleep);
+	u64 last_update_time_copy;
+	u64 last_update_time;
 
-	cfs_rq->runnable_load_avg -= se->avg.load_avg_contrib;
-	cfs_rq->utilization_load_avg -= se->avg.utilization_avg_contrib;
-	if (sleep) {
-		cfs_rq->blocked_load_avg += se->avg.load_avg_contrib;
-		se->avg.decay_count = atomic64_read(&cfs_rq->decay_counter);
-	} /* migrations, e.g. sleep=0 leave decay_count == 0 */
+	do {
+		last_update_time_copy = cfs_rq->load_last_update_time_copy;
+		smp_rmb();
+		last_update_time = cfs_rq->avg.last_update_time;
+	} while (last_update_time != last_update_time_copy);
+
+	return last_update_time;
+}
+#else
+static inline u64 cfs_rq_last_update_time(struct cfs_rq *cfs_rq)
+{
+	return cfs_rq->avg.last_update_time;
+}
+#endif
+
+/*
+ * Task first catches up with cfs_rq, and then subtract
+ * itself from the cfs_rq (task must be off the queue now).
+ */
+void remove_entity_load_avg(struct sched_entity *se)
+{
+	struct cfs_rq *cfs_rq = cfs_rq_of(se);
+	u64 last_update_time;
+
+	/*
+	 * Newly created task or never used group entity should not be removed
+	 * from its (source) cfs_rq
+	 */
+	if (se->avg.last_update_time == 0)
+		return;
+
+	last_update_time = cfs_rq_last_update_time(cfs_rq);
+
+	__update_load_avg(last_update_time, cpu_of(rq_of(cfs_rq)), &se->avg, 0, 0, NULL);
+	atomic_long_add(se->avg.load_avg, &cfs_rq->removed_load_avg);
+	atomic_long_add(se->avg.util_avg, &cfs_rq->removed_util_avg);
 }
 
 /*
